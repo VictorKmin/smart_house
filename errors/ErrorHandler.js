de21fcce3a1580a6d6da89c2a0a2b42b1b524a3e
VictorKmin <kminvictor@gmail.com>
2019-09-14 00:00:28 +0300

    Add errors

---
+++ errors/ErrorHandler.js
@@ -0,0 +1,10 @@
+class ErrorHandler extends Error{
+    constructor(msg, status, controller) {
+        super(msg);
+        this.name = 'Controller Error';
+        (this.parent) ? this.message = this.parent.sqlMessag : this.message = msg;
+        this.status = status;
+        this.controller = controller;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
